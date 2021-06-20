3ced4ff2101070fe893ab271394b69de95471e7e
Moonshadow <last_enemy@naver.com>
2021-06-20 10:18:50 +0900

    greeting

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const loginForm = document.querySelector('#login-form');
-const loginInput = loginForm.querySelector('input');
+const loginInput = document.querySelector('#login-form input');
+const greeting = document.querySelector('#greeting');
 
 function onLoginSubmit(event) {
   event.preventDefault();
+  loginForm.classList.add('hidden');
   const uesername = loginInput.value;
+  greeting.innerHTML = uesername;
+  greeting.classList.remove('hidden');
 }
 
 loginForm.addEventListener('submit', onLoginSubmit);

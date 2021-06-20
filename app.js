434bb46de07e5417ce97767a6745f3ed1641ac96
Moonshadow <last_enemy@naver.com>
2021-06-20 10:35:59 +0900

    save user in local storage

--- app.js
+++ app.js
@@ -3,13 +3,26 @@ const loginInput = document.querySelector('#login-form input');
 const greeting = document.querySelector('#greeting');
 
 const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'userName';
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  const uesername = loginInput.value;
-  greeting.innerHTML = `Hello, ${uesername}`;
+  const userName = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, userName);
+  paintGreetings(userName);
+}
+
+function paintGreetings(userName) {
+  greeting.innerText = `Hello, ${userName}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener('submit', onLoginSubmit);
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+if (savedUserName === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener('submit', onLoginSubmit);
+} else {
+  paintGreetings(savedUserName);
+}

e7d141924d40541706d920fb50f5c88ef52b13d1
Moonshadow <last_enemy@naver.com>
2021-06-24 18:42:40 +0900

    add geolocation and weather

---
+++ js/weather.js
@@ -0,0 +1,40 @@
+function onGeoOk(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const API_KEY = '53bf65d947502bb89f2eee975ed950cb';
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector('#weather');
+      const weatherIcon = document.querySelector('#weather-icon');
+      const citySpan = weather.querySelector('span');
+      const tempContainer = document.querySelector('#temp');
+      const weatherIcons = {
+        01: 'far fa-sun',
+        02: 'fas fa-cloud-sun',
+        03: 'fas fa-cloud',
+        04: 'fas fa-cloud-meatball',
+        09: 'fas fa-cloud-sun-rain',
+        10: 'fas fa-cloud-showers-heavy',
+        11: 'fas fa-poo-storm',
+        13: 'far fa-snowflake',
+        50: 'fas fa-smog',
+      };
+      weather.append(
+        weatherIcon.classList.add(
+          weatherIcons[parseInt(data.weather[0].icon.substr(0, 2))].split(
+            ' '
+          )[0],
+          weatherIcons[parseInt(data.weather[0].icon.substr(0, 2))].split(
+            ' '
+          )[1]
+        )
+      );
+      citySpan.innerText = data.name;
+    });
+}
+function onGeoError() {
+  alert("Can't find you.");
+}
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
